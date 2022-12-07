ec30df3a0d20804a20458b553f2cb4d9cda6a41d
nobur7839 <nobur7839@8R4YP52.Student.UGDSB.ED>
2022-12-07 13:33:58 -0500

    New question edit

--- src/studybot1000/Quiz.java
+++ src/studybot1000/Quiz.java
@@ -1,62 +1,62 @@
-
 package studybot1000;
 
 import java.util.ArrayList;
 
 public class Quiz {
+
     private ArrayList<Question> questions;
     private int score;
     private int size;
 
-    private Question getQuestion(int index) {
+    public Question getQuestion(int index) {
         return questions.get(index);
     }
-    
-    private void setQuestion(int index, Question question){
+
+    public void setQuestion(int index, Question question) {
         questions.add(index, question);
     }
-    
-    private int getScore() {
+
+    public int getScore() {
         return score;
     }
-    
-    private void setScore(int score) {
+
+    public void setScore(int score) {
         this.score = score;
     }
-    
-    private int getSize() {
+
+    public int getSize() {
         return size;
     }
-    
-    private void addQuestion(Question q) {
+
+    public void addQuestion(Question q) {
         questions.add(q);
     }
-    
-    private void removeQuestion(Question q) {
+
+    public void removeQuestion(Question q) {
         questions.remove(q);
     }
-    
-    private void addScore() {
-        score ++;
+
+    public void addScore() {
+        score++;
     }
-    
-    private void removeScore() {
-        score --;
+
+    public void removeScore() {
+        score--;
     }
-    
-    private void eraseScore() {
+
+    public void eraseScore() {
         score = 0;
     }
-    
+
     public Quiz() {
         questions = null;
         score = 0;
         size = 0;
     }
-    
+
     public Quiz(ArrayList<Question> q) {
         this();
         questions = q;
     }
-    
+
 }
